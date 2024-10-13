45f353f8c6ce201c37cee342f5ae8476c745411b
YoMoM829 <AnandVannalath@gmail.com>
2024-10-13 19:36:43 +1100

    Added JS code for new map chart

--- JS/Ass2.js
+++ JS/Ass2.js
@@ -17,3 +17,105 @@ vegaEmbed('#energy_exports', spec).then(function(result) {
 var spec = "VEGA/energy_percentage_type.vg.json";
 vegaEmbed('#energy_map', spec).then(function(result) {
 }).catch(console.error);
+
+var fuelSelect = document.querySelector("#fuelType");
+
+fuelSelect.addEventListener("change", function() {
+    var fuelType = fuelSelect.value; 
+    let path = ""
+
+    if (fuelType === "Coal") {
+        path = "energy_cons_coal_time_state.csv";
+    } else if (fuelType === "Gas") {
+        path = "energy_cons_gas_time_state.csv";
+    } else if (fuelType === "Renewables") {
+        path = "energy_cons_renewables_time_state.csv"
+    } else if (fuelType === "Oil") {
+        path = "energy_cons_oil_time_state.csv"
+    }
+
+    var spec = {
+        "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+        "title": {
+          "text": "Fuel Usage by Percentage across Australian States",
+          "fontSize": 40
+        },
+        "width": 600,
+        "height": 300,
+        "projection": {"type": "mercator"},
+        "params": [
+          {
+            "name": "FuelType",
+            "value": "Coal",
+            "bind": {
+              "input": "select",
+              "options": ["Coal", "Renewables", "Gas", "Oil"]
+            }
+          }
+        ],
+        "data": {
+          "url": "https://raw.githubusercontent.com/amishmishra27/FIT3179_A2/refs/heads/main/js/aus_boundaries.topojson",
+          "format": {
+            "type": "topojson",
+            "feature": "STE_2021_AUST_GDA2020"
+          }
+        },
+        "transform": [
+          {
+            "lookup": "properties.STE_NAME21",
+            "from": {
+              "data": {
+                "url": "https://raw.githubusercontent.com/YoMoM829/FIT3179-/refs/heads/main/DATA/energy_percentage_map.csv"
+              },
+              "key": "States",
+              "fields": ["Coal", "Renewables", "Gas", "Oil"]
+            }
+          },
+          {
+            "calculate": "datum[FuelType]",
+            "as": "FuelUsage"
+          }
+        ],
+        "layer": [
+          {
+            "mark": {
+              "type": "geoshape",
+              "fill": "lightgray",
+              "stroke": "black"
+            }
+          },
+          {
+            "mark": {
+              "type": "geoshape",
+              "stroke": "white"
+            },
+            "encoding": {
+              "color": {
+                "field": "FuelUsage",
+                "type": "quantitative",
+                "title": "Fuel Usage (%)",
+                "scale": {
+                  "domain": [0, 20, 40, 60, 80, 100],
+                  "range": ["#fc9292", "#b00202"]
+                }
+              },
+              "tooltip": [
+                {
+                  "field": "properties.STE_NAME21",
+                  "type": "nominal",
+                  "title": "State"
+                },
+                {
+                  "field": "FuelUsage",
+                  "type": "quantitative",
+                  "format": ".2f",
+                  "title": "Fuel Usage (%)"
+                }
+              ]
+            }
+          }
+        ]
+    }
+    vegaEmbed('#energy_map', spec).then(function(result) {
+    }).catch(console.error);
+}); 
